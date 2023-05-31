e485e0d6e949f008bbce6dca8c9735306e94039e
yeahimjt <80553412+yeahimjt@users.noreply.github.com>
2023-05-31 02:50:03 -0500

    Updating skill tease position

--- client/src/containers/SkillTease.js
+++ client/src/containers/SkillTease.js
@@ -7,7 +7,7 @@ import { ThemeContext } from '../App'
 const SkillTease = () => {
   const {theme} = useContext(ThemeContext)
   return (
-    <div className={theme==="light"? "max-w-[1158px]  flex justify-center w-full  bg-[color:var(--skill-tease)]  absolute -bottom-[500px] tablet:bottom-12 py-8 items-center flex-wrap z-40 gap-8 gap-y-2 animate-grow opacity-0 animation-delay-3000 shadow-2xl  " : "max-w-[1158px] flex justify-center bg-[color:var(--skill-tease-dark)] w-full  absolute -bottom-96 tablet:bottom-8 py-8 items-center animate-grow flex-wrap z-40 gap-8 gap-y-2 shadow-2xl"}>
+    <div className={theme==="light"? "max-w-[1158px]  flex justify-center w-full  bg-[color:var(--skill-tease)]  absolute mb-8 tablet:bottom-12 py-8 items-center flex-wrap z-40 gap-8 gap-y-2 animate-grow opacity-0 animation-delay-3000 shadow-2xl  " : "max-w-[1158px] flex justify-center bg-[color:var(--skill-tease-dark)] w-full  absolute -bottom-96 tablet:bottom-8 py-8 items-center animate-grow flex-wrap z-40 gap-8 gap-y-2 shadow-2xl"}>
         <DiReact className="text-[color:var(--light-blue)] icon text-big" />
         <SiTailwindcss className="text-[color:var(--light-blue)] icon text-big" />
         <DiMysql className="text-[color:var(--light-blue)] icon text-big" />
