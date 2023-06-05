041bf7b18cdc26228bb63bb78e4e797b6b7bbca7
yeahimjt <80553412+yeahimjt@users.noreply.github.com>
2023-06-05 00:21:21 -0500

    Reducing z-index of skill tease

--- client/src/containers/SkillTease.js
+++ client/src/containers/SkillTease.js
@@ -7,7 +7,7 @@ import { ThemeContext } from '../App'
 const SkillTease = () => {
   const {theme} = useContext(ThemeContext)
   return (
-    <div className={theme==="light"? "max-w-[1158px]  flex justify-center w-[70%] smallest:w-full  bg-[color:var(--skill-tease)]  absolute mb-8 -bottom-16 tablet:-bottom-20 py-8 items-center flex-wrap z-30 gap-8 gap-y-2 animate-grow opacity-0 animation-delay-3000 shadow-2xl  " : "max-w-[1158px] flex justify-center bg-[color:var(--skill-tease-dark)] w-[70%] smallest:w-full absolute mb-8 -bottom-16 tablet:-bottom-20 py-8 items-center animate-grow flex-wrap z-30 gap-8 gap-y-2 shadow-2xl"}>
+    <div className={theme==="light"? "max-w-[1158px]  flex justify-center w-[70%] smallest:w-full  bg-[color:var(--skill-tease)]  absolute mb-8 -bottom-16 tablet:-bottom-20 py-8 items-center flex-wrap z-20 gap-8 gap-y-2 animate-grow opacity-0 animation-delay-3000 shadow-2xl  " : "max-w-[1158px] flex justify-center bg-[color:var(--skill-tease-dark)] w-[70%] smallest:w-full absolute mb-8 -bottom-16 tablet:-bottom-20 py-8 items-center animate-grow flex-wrap z-20 gap-8 gap-y-2 shadow-2xl"}>
         <DiReact className="text-[color:var(--light-blue)] icon text-big" />
         <SiTailwindcss className="text-[color:var(--light-blue)] icon text-big" />
         <DiMysql className="text-[color:var(--light-blue)] icon text-big" />
