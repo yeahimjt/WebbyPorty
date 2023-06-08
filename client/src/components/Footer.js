2362436a0db03f31ca90cee0c60a9366baf49229
yeahimjt <80553412+yeahimjt@users.noreply.github.com>
2023-06-08 01:18:50 -0500

    Updating footer contact button, and theme switch

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,11 +4,9 @@ import { ThemeContext } from '../App'
 const Footer = () => {
   const {theme} = useContext(ThemeContext)
   return (
-    <footer className={theme==="light"?"bg-[color:var(--skill-tease)] pt-16 pb-8":"bg-[color:var(--skill-tease-dark)] pt-16 pb-8 text-white"}>
-        <div className="flex flex-col justify-center items-center gap-4">
-            <h1 className="font-paragraph text-med">Interested in starting a conversation?</h1>
-            <a className="font-heading text-sm bg-[color:var(--black)] px-4 py-2 rounded-full text-white hover:scale-105 transition-all" href="mailto:yeahimjt@gmail.com">Contact me</a>
-        </div>
+    <footer className={theme==="light"?"bg-[color:var(--skill-tease)] pt-16 pb-8 flex flex-col justify-center items-center":"bg-[color:var(--skill-tease-dark)] pt-16 pb-8 text-white flex flex-col justify-center items-center"}>
+        <h1 className="font-paragraph text-med">Interested in starting a conversation?</h1>
+        <a className={theme==="light"?"font-heading text-sm bg-[color:var(--black)] px-4 py-2 rounded-full text-white hover:scale-105 transition-all":"font-heading text-sm bg-white px-4 py-2 rounded-full text-black hover:scale-105 transition-all"} href="mailto:yeahimjt@gmail.com">Contact me</a>
         <p className="text-center mt-8 italic text-xs">I really appreciate the time you've taken to explore my website!</p>
     </footer>
   )
